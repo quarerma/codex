d486bad02fcefa862a7d4fd1208e081caf4b0135
Gabriel Oliveira Quaresma <gabriel.oliveira.quaresma@gmail.com>
2024-06-21 14:54:45 -0300

    edit skill, add alterations, change training level

--- src/character/aux_services/character.skills.service.ts
+++ src/character/aux_services/character.skills.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { Atribute } from '@prisma/client';
 import { DataBaseService } from 'src/database/database.service';
 import { SkillService } from 'src/skill/skill.service';
-import { SkillJson } from 'src/types/skill-relates-types';
+import { SkillAlterationObject, SkillJson, TrainingLevel } from 'src/types/skill-relates-types';
 
 @Injectable()
 export class CharacterSkillsService {
@@ -20,11 +21,131 @@ export class CharacterSkillsService {
         name: skill.name,
         atribute: skill.atribute,
         trainingLevel: 'none',
+        value: 0,
         alterations: [],
       } as SkillJson;
       characterSkills.push(skillJson);
     }
 
     return characterSkills;
+  }
+
+  async editCharacterSkillAtribute(characterId: string, skillName: string, atribute: Atribute) {
+    try {
+      const character = await this.dataBaseService.character.findUnique({
+        where: { id: characterId },
+        select: {
+          skills: true,
+        },
+      });
+
+      const skills = character.skills as SkillJson[];
+
+      const skillIndex = skills.findIndex((skill) => skill.name === skillName);
+
+      if (skillIndex === -1) {
+        throw new Error('Skill not found');
+      }
+
+      skills[skillIndex].atribute = atribute;
+
+      await this.dataBaseService.character.update({
+        where: { id: characterId },
+        data: {
+          skills: {
+            set: skills,
+          },
+        },
+      });
+    } catch (error) {
+      throw new Error('Error editing character skill atribute');
+    }
+  }
+
+  async editCharacterSkillTraining(characterId: string, skillName: string, trainingLevel: TrainingLevel) {
+    try {
+      const character = await this.dataBaseService.character.findUnique({
+        where: { id: characterId },
+        select: {
+          skills: true,
+        },
+      });
+
+      const skills = character.skills as SkillJson[];
+
+      const skillIndex = skills.findIndex((skill) => skill.name === skillName);
+
+      if (skillIndex === -1) {
+        throw new Error('Skill not found');
+      }
+
+      skills[skillIndex].trainingLevel = trainingLevel;
+
+      switch (trainingLevel) {
+        case 'none':
+          skills[skillIndex].value = 0;
+          break;
+        case 'trained':
+          skills[skillIndex].value = 5;
+          break;
+        case 'veteran':
+          skills[skillIndex].value = 10;
+          break;
+        case 'expert':
+          skills[skillIndex].value = 15;
+          break;
+        default:
+          throw new Error('Invalid training level');
+      }
+
+      for (const alteration of skills[skillIndex].alterations) {
+        skills[skillIndex].value += alteration.value;
+      }
+
+      await this.dataBaseService.character.update({
+        where: { id: characterId },
+        data: {
+          skills: {
+            set: skills,
+          },
+        },
+      });
+    } catch (error) {
+      throw new Error('Error editing character skill training');
+    }
+  }
+
+  async addSkillAlteration(characterId: string, skillName: string, alteration: SkillAlterationObject) {
+    try {
+      const character = await this.dataBaseService.character.findUnique({
+        where: { id: characterId },
+        select: {
+          skills: true,
+        },
+      });
+
+      const skills = character.skills as SkillJson[];
+
+      const skillIndex = skills.findIndex((skill) => skill.name === skillName);
+
+      if (skillIndex === -1) {
+        throw new Error('Skill not found');
+      }
+
+      skills[skillIndex].alterations.push(alteration);
+
+      skills[skillIndex].value += alteration.value;
+
+      await this.dataBaseService.character.update({
+        where: { id: characterId },
+        data: {
+          skills: {
+            set: skills,
+          },
+        },
+      });
+    } catch (error) {
+      throw new Error('Error adding skill alteration');
+    }
   }
 }
--- src/types/skill-relates-types.ts
+++ src/types/skill-relates-types.ts
@@ -5,6 +5,7 @@ export type TrainingLevel = 'none' | 'trained' | 'veteran' | 'expert';
 export type SkillJson = {
   name: string;
   atribute: Atribute;
+  value: number;
   trainingLevel: TrainingLevel;
   alterations: SkillAlterationObject[];
 };
