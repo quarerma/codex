60fbdaba6f42b0b6fd626d0be684dd879efef214
Gabriel Oliveira Quaresma <114524484+quarerma@users.noreply.github.com>
2024-12-30 17:30:17 -0300

    change frontend domain

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'http://127.0.0.1:5173',
+    origin: 'https://devgabrielquaresma-codex.netlify.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
