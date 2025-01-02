6bdaacbe2e53973572a54965fe493c6aee9abb23
Gabriel Oliveira Quaresma <114524484+quarerma@users.noreply.github.com>
2025-01-02 20:33:01 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'https://devgabrielquaresma-codex.netlify.app',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
